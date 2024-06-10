6418ebb378c4e5281eab3d5fc12e1b6769077a3d
Cody Wilson <cody.wilson9495@gmail.com>
2024-06-10 16:16:23 -0700

    chore: Set Nuxt SSR to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import daisyui from 'daisyui';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss',
